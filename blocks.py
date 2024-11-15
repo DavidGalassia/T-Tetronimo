02bf2b45cb57a6055f570f75b08deae0cc2f4179
La Gala <daarismenv@eafit.edu.co>
2024-11-14 19:18:21 -0500

    Update blocks.py

--- blocks.py
+++ blocks.py
@@ -3,14 +3,14 @@ from position import Position
 
 class LBlock(Block):
 	def __init__(self):
-	super().__init__(id = 1)
-        self.cells = {
-            0: [Position(0, 1), Position(1, 0), Position(1, 1), Position(1, 2)],
-            1: [Position(0, 1), Position(1, 1), Position(1, 2), Position(2, 1)],
-            2: [Position(1, 0), Position(1, 1), Position(1, 2), Position(2, 1)],
-            3: [Position(0, 1), Position(1, 0), Position(1, 1), Position(2, 1)]
-        }
-        self.move(0, 3)
+		super().__init__(id = 1)
+		self.cells = {
+			0: [Position(0, 1), Position(1, 0), Position(1, 1), Position(1, 2)],
+			1: [Position(0, 1), Position(1, 1), Position(1, 2), Position(2, 1)],
+			2: [Position(1, 0), Position(1, 1), Position(1, 2), Position(2, 1)],
+			3: [Position(0, 1), Position(1, 0), Position(1, 1), Position(2, 1)]
+		}
+		self.move(0, 3)
 
 class JBlock(Block):
     def __init__(self):
